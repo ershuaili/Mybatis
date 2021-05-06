e147aa10875d30a994d9be6e7b2e7c33f9b74588
shuai <liershuai.cn@gmail.com>
2021-05-06 19:59:59 +0800

    master

--- mybatis-02/src/main/java/com/example/pojo/User.java
+++ mybatis-02/src/main/java/com/example/pojo/User.java
@@ -12,13 +12,15 @@ import lombok.ToString;
  * @author 李二帅
  * @version 1.0
  * @Date 2021/05/05 下午 11:21
+ *
+ * 实体类类提供getter和setter方法
  */
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-class User {
+public class User {
     private int id;
     private String name;
     private String pwd;
