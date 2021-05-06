2f175a5b386d037543eef10f00afa2b96611187f
shuai <liershuai.cn@gmail.com>
2021-05-06 20:00:15 +0800

    master

---
+++ mybatis-02/src/main/java/com/example/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package com.example.mapper;
+
+import com.example.pojo.User;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author 李二帅
+ * @version 1.0
+ * @Date 2021/05/06 下午 4:23
+ */
+public interface UserMapper {
+    /**
+     * shuai
+     * @return List
+     */
+    List<User> getUserList();
+}
