b6fa77e004ec3063915e58033f79d9d9dc4c6cd5
shuai <liershuai.cn@gmail.com>
2021-05-10 17:38:22 +0800

    master

--- mybatis-02/src/test/java/com/example/pojo/UserTest.java
+++ mybatis-02/src/test/java/com/example/pojo/UserTest.java
@@ -1,6 +1,13 @@
 package com.example.pojo;
 
+import com.example.entity.User;
+import com.example.mapper.UserMapper;
+import com.example.service.impl.MybatisImpl;
 import junit.framework.TestCase;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -9,6 +16,34 @@ import junit.framework.TestCase;
  * @version 1.0
  * @Date 2021/05/05 下午 11:23
  */
-public class UserTest extends TestCase {
+public class UserTest {
+
+    @Test
+    public void test() {
+
+        //获得SqlSession对象
+        SqlSession sqlSession = MybatisImpl.getSqlSession();
+
+        try {
+            //执行SQL
+            //方式一 推荐
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            List<User> userList = userMapper.getUserList();
+
+            //方式二 了解
+//            List<User> userList = sqlSession.selectList("com.glp.dao.UserMapper.getUserList");
+
+            for (User user : userList) {
+                System.out.println(user);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //关闭sqlSession
+            sqlSession.close();
+        }
+    }
+
 
 }
